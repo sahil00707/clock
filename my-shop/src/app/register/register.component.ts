476889dbb9af32a2ee05b646c4db761db7cfc085
Sahil <ajmerisahil94@gmail.com>
2023-02-28 15:53:42 +0530

    routing

--- my-shop/src/app/register/register.component.ts
+++ my-shop/src/app/register/register.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild ,OnInit,AfterContentInit} from '@angular/core';
 import { Regi } from '../regi';
 import { MyserviceService } from '../services/myservice.service';
+import {HttpClient} from '@angular/common/http'
+import { Router } from '@angular/router';
+import { ChildComponent } from './child/child.component';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  @ViewChild('hello')hello: any;
+  @ViewChild(ChildComponent)p: any;
+
   
-  register(data:Regi){
-this.service.saveData(data);
-//this.service.isLoggedIn.next(true)
-  }
-  constructor(private service:MyserviceService){
+
+
+  constructor(private service:MyserviceService,private router:Router){
 
   }
+  ngOnInit(){
+  }
+  ngAfterViewInit(){
+   this.hello.nativeElement.style.color="red";
+   console.log(this.p);
+   
+
+ }
 }
